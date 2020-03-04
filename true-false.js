1bd865589b8b1ef8e6f21108d07d97a8c2f56de9
aminulsarkerbd <55542703+aminulsarkerbd@users.noreply.github.com>
2020-03-04 09:50:47 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,30 @@
+//Falsy:
+
+//false
+//0               
+//""              
+//undefined       
+//null            
+//NaN             
+
+//Truthy:
+
+//"0", " ", [], {}
+
+
+var age = 4;
+if (age > 0) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+
+var name = "";
+
+if (name) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
