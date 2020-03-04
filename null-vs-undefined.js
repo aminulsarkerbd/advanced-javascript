8a512af63dfde2d92247cf5730f2d55b7d76088a
aminulsarkerbd <55542703+aminulsarkerbd@users.noreply.github.com>
2020-03-04 11:06:28 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,32 @@
+//Undefined:
+
+let name; // value missing
+console.log(name);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    //return missing
+}
+const result = add(5, 7);
+console.log(result);
+
+const atik = { id: 222, phone: 01911 };
+console.log(atik.address);
+
+let sun = undefined; //JS reserved word;
+console.log(sun);
+
+let ages = [3, 5, 7];
+console.log(ages[10]);
+
+
+
+
+
+//NaN:
+
+function add2(num3, num4) {
+    return num3 + num4;
+}
+const result2 = add2(3);
+console.log(result2);
